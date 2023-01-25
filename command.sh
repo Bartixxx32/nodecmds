756a86cbfa12daa47aec9d3a2541566a3fb062be
Bartixxx32 <mcbplay1@gmail.com>
2023-01-25 21:08:06 +0100

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,2 +1,7 @@
 docker system prune -a -f
 docker system prune --volumes -f
+service nginx stop
+sleep 5
+certbot renew --quiet --deploy-hook "systemctl restart nginx"
+sleep 5
+sudo service nginx start
