d29f57e8a6acda68bdfc16b1f52e84f746f8ddbf
Bartixxx32 <mcbplay1@gmail.com>
2023-08-11 16:54:29 +0200

    Update command.sh

--- command.sh
+++ command.sh
@@ -4,4 +4,7 @@ service nginx stop
 sleep 5
 certbot renew --quiet --deploy-hook "systemctl restart nginx"
 sleep 5
-sudo service nginx start
+service nginx start
+systemctl stop wings
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+chmod u+x /usr/local/bin/wings
