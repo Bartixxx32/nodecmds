dabb97b41a3c51deab554d7f67c72f647dcd5d34
Bartixxx32 <mcbplay1@gmail.com>
2023-12-25 19:18:12 +0100

    Update command.sh

--- command.sh
+++ command.sh
@@ -8,3 +8,4 @@ service nginx start
 systemctl stop wings
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 chmod u+x /usr/local/bin/wings
+systemctl start wings
